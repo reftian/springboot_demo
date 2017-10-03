3a373a1aee3f18285a5e56c0bfdd23d5a6ca7e8c
ref123 <ref.tian@hotmail.com>
2017-10-03 21:05:36 +0800

    22

--- src/main/java/com/ref/service/javaservice.java
+++ src/main/java/com/ref/service/javaservice.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.math.BigInteger;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * Created by admi on 2017/9/29.
@@ -13,8 +16,37 @@ import javax.annotation.PostConstruct;
 @Slf4j
 public class javaservice {
 
+    final String finalparam="这个是finale数据";
     @PostConstruct
     public void init() {
         log.info("测试服务启动。。。。");
+        System.out.println("hello word!");
+
+        double douparam=16.54;
+        String intstring = "16";
+        String[] array={"ddd","3333"};
+        BigInteger bi= BigInteger.valueOf(100);
+        log.info("输出的doubale数据为:"+douparam);
+        log.info("平方根："+Math.sqrt(douparam));
+        log.info("舍入："+ Math.round(douparam));
+        log.info("类型："+Tyle.t_c);
+        log.info("String Substring:"+finalparam.substring(2,3));
+        log.info("Cast转换："+ ((int) douparam));
+//        log.info("Cast转换："+ (((int) intstring)));
+        log.info("char at:{}",intstring.charAt(1));
+        log.info("offsetbycodepoint:{}",finalparam.offsetByCodePoints(1,2));
+        log.info("biginteger:{}",bi);
+        for (String i:array) {
+            log.info("for输出数组信息{}",i);
+        }
+        GregorianCalendar   now= new GregorianCalendar();
+        int month = now.get(Calendar.MONTH);
+        log.info("当前的月为:{}",month);
+    }
+
+
+    enum Tyle{
+        t_g,
+        t_c
     }
 }
